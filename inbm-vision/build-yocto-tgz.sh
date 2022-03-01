705aae82faa79964b65d320d44af85b9ac9efd5a
Natalie Gaston <natalie.gaston@intel.com>
2022-03-01 14:32:34 -0800

    IT not stopping on AppArmor Error

--- inbm-vision/build-yocto-tgz.sh
+++ inbm-vision/build-yocto-tgz.sh
@@ -2,6 +2,5 @@
 set -euxo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-docker run --rm --privileged registry.hub.docker.com/multiarch/qemu-user-static --reset -p yes
 
 "$DIR"/dockerfiles/build-Dockerfile.sh Yocto-tgz
