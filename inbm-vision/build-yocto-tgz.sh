0798c85217d2b9f7165ead2f08ac57485a75bb81
Natalie Gaston <natalie.gaston@intel.com>
2022-03-01 17:46:03 -0800

    revert change

--- inbm-vision/build-yocto-tgz.sh
+++ inbm-vision/build-yocto-tgz.sh
@@ -2,5 +2,6 @@
 set -euxo pipefail
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+docker run --rm --privileged registry.hub.docker.com/multiarch/qemu-user-static --reset -p yes
 
 "$DIR"/dockerfiles/build-Dockerfile.sh Yocto-tgz
