5751ff4f55b14e9f0cb62f743349e2b5fd0c9a98
Siva472 <101618023+Siva472@users.noreply.github.com>
2022-04-26 20:37:49 +0530

    Update test_publisher.py

--- inbm/cloudadapter-agent/tests/unit/agent/test_publisher.py
+++ inbm/cloudadapter-agent/tests/unit/agent/test_publisher.py
@@ -172,7 +172,7 @@ class TestPublisher(unittest.TestCase):
         
         
         self.QUERY_ARGUMENTS_ALL = {
-            "option": "all",
+            "option": "all", "guid",
         }
 
         self.QUERY_ALL = ('<?xml version="1.0" encoding="UTF-8"?>'
