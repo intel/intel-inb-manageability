c4ee31b1ba7be7ee72a75403a14d610f342713d4
siva472 <thrivendrax.siva.krishnareddy.gangireddy@intel.com>
2022-04-26 19:26:05 +0530

    Testing query option

--- inbm/cloudadapter-agent/tests/unit/agent/test_publisher.py
+++ inbm/cloudadapter-agent/tests/unit/agent/test_publisher.py
@@ -175,12 +175,12 @@ class TestPublisher(unittest.TestCase):
                         '<type>cmd</type>'
                         '<cmd>query</cmd>'
                         '<query>'
-                            '<option>hw</option>'
+                            '<option>fw</option>'
                         '</query>'
                       '</manifest>')  # noqa: E127
 
         self.QUERY_ARGUMENTS = {
-            "option": "hw",
+            "option": "fw",
         }
 
     def test_publish_manifest_succeed(self):
@@ -344,7 +344,7 @@ class TestPublisher(unittest.TestCase):
             failed = True
         assert failed
 
-    def test_query_hw(self):
+    def test_query_fw(self):
         arguments = self.QUERY_ARGUMENTS
 
         message = self.publisher.publish_query(**arguments)
