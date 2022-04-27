a18a30dba7c0a369d63ad261e1552370aad486db
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-04-27 16:24:49 +0530

    Added logs to debug the RTC 497932

--- inbm/dispatcher-agent/dispatcher/packagemanager/package_manager.py
+++ inbm/dispatcher-agent/dispatcher/packagemanager/package_manager.py
@@ -101,8 +101,8 @@ def is_enough_space_to_download(uri: CanonicalUri,
         with requests.get(uri.value, auth=auth, verify=get_platform_ca_certs(), stream=True) as response:
             logger.info("Test==================line 101")
             logger.debug(response)
-            #logger.debug(response.raise_for_status())
-            response.raise_for_status()
+            print(response.raise_for_status())
+            #response.raise_for_status()
             logger.info("Test==================line 103")
             # Read Content-Length header
             try:
