99fb4df1cf7dfe4adeba567a2f95b3c2df754f55
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-04-28 19:09:59 +0530

    Added logs to debug the RTC 497932

--- inbm/dispatcher-agent/dispatcher/sota/setup_helper.py
+++ inbm/dispatcher-agent/dispatcher/sota/setup_helper.py
@@ -77,7 +77,8 @@ class DebianBasedSetupHelper(SetupHelper):
         """
         logger.debug("Test Print ================================78")
         logger.debug(payload)
-        temp_payload = payload.split(':', 1)[1].strip(' \t\n\r')
+        #temp_payload = payload.split(':', 1)[1].strip(' \t\n\r')
+        temp_payload = payload
 
         logger.debug(temp_payload)
         # solves bug 38278
