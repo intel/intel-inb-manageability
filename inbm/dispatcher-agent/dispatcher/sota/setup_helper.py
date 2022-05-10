978c917216a78aec1b036eed541f8e1ccf4a52c8
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-05-10 13:41:38 +0530

    Added logs to debug the RTC 497932

--- inbm/dispatcher-agent/dispatcher/sota/setup_helper.py
+++ inbm/dispatcher-agent/dispatcher/sota/setup_helper.py
@@ -77,9 +77,11 @@ class DebianBasedSetupHelper(SetupHelper):
         logger.debug("")
         logger.debug("===========================================Tets print 78=======================================")
         logger.debug(payload)
-        test_payload = payload
-        #temp_payload = payload.split(':', 1)[1].strip(' \t\n\r')
-        temp_payload = test_payload.strip()
+        #test_payload = payload
+        if not payload.startswith('http'):
+            temp_payload = payload.split(':', 1)[1].strip(' \t\n\r')
+        else:
+            temp_payload = payload.strip()
 
         logger.debug(temp_payload)
         # solves bug 38278
