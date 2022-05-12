4120b0a0e1d4fd833294e200e93c8d2bdcfe954f
Gavin Lewis <gavin.b.lewis@intel.com>
2022-05-12 15:10:54 -0700

    fix

--- inbm/integration-reloaded/test/general/UPDATE_SYSTEM.sh
+++ inbm/integration-reloaded/test/general/UPDATE_SYSTEM.sh
@@ -12,7 +12,7 @@ apt-mark hold linux-generic linux-image-generic linux-headers-generic
 
 # Update the system here to standardize/simplify further test results.
 apt-get update
-apt-get -y upgrade || ( apt-get -f install && apt-get -y upgrade )
+apt-get -y upgrade || ( apt-get -y -f install && apt-get -y upgrade )
 apt-get clean
 
 # Restart docker to make sure it's running
