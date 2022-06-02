0745811c74238d68107fa29f46fd8007dbf2a711
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-06-02 11:47:42 +0530

    Added uninstall command for for AOTA update

--- inbm/dispatcher-agent/tests/unit/aota/test_aota.py
+++ inbm/dispatcher-agent/tests/unit/aota/test_aota.py
@@ -616,7 +616,7 @@ class TestAOTA(TestCase):
     @patch('dispatcher.aota.factory.detect_os', return_value='CentOS')
     def test_application_centos_driver_update_raise_pass(self, detect_os, mock_detect_os, create_repo, listdir, mock_move,
                                                          support_driver, run):
-        aota = self._build_aota(cmd='update', app_type='application', uri="http://example.com")
+        aota = self._build_aota(cmd='update', app_type='application', uri="http://example.com/testt.rpm")
         self.assertIsNone(aota.run())
 
 
