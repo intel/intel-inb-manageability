db285469f9631bea80c3b3594a1fd98918bb4ca1
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-06-13 11:48:54 +0530

    Adding new test case for file type check

--- inbm/dispatcher-agent/tests/unit/aota/test_aota.py
+++ inbm/dispatcher-agent/tests/unit/aota/test_aota.py
@@ -606,8 +606,13 @@ class TestAOTA(TestCase):
         aota = self._build_aota(cmd='update', app_type='application',
                                 uri="http://example.com", device_reboot="Yes")
         self.assertRaises(AotaError, aota.run)
-
-
+  
+    @patch('dispatcher.aota.application_command.is_inside_container', return_value=False)
+    @patch('dispatcher.aota.checker.check_url')
+    def test_application_centos_driver_update_raise_error_if_file_is_not_rpm(self, check_url, mock_detect_os):
+        aota = self._build_aota(cmd='update', app_type='application',
+                                uri="http://example.com")
+        self.assertRaises(AotaError, aota.run)
 
 
     @patch('inbm_common_lib.shell_runner.PseudoShellRunner.run', return_value=("", "", 0))
