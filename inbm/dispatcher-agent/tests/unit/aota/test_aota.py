9ac4b7c42f93d24b0d15c1abc2d8e00ad33c9702
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-06-14 18:34:12 +0530

    Adding new test case for file type check

--- inbm/dispatcher-agent/tests/unit/aota/test_aota.py
+++ inbm/dispatcher-agent/tests/unit/aota/test_aota.py
@@ -624,7 +624,7 @@ class TestAOTA(TestCase):
 
     #@patch("dispatcher.packagemanager.package_manager.get", return_value=Result(200, "ok"))
     #@patch('dispatcher.aota.application_command.get', return_value=Result(200, "ok"))
-    @patch('inbm_common_lib.shell_runner.PseudoShellRunner.run', return_value=("", "", 0))
+    #@patch('inbm_common_lib.shell_runner.PseudoShellRunner.run', return_value=("", "", 0))
     #@patch('dispatcher.aota.application_command.Application.identify_package', return_value=SupportedDriver.XLINK.value)
     #@patch('dispatcher.aota.application_command.move_file')
     #@patch('os.listdir', return_value=[])
