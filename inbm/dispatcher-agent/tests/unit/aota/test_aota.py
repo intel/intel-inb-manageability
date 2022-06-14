5ee1b163a685852ae92f2190f8f6562189979774
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-06-14 18:23:26 +0530

    Adding new test case for file type check

--- inbm/dispatcher-agent/tests/unit/aota/test_aota.py
+++ inbm/dispatcher-agent/tests/unit/aota/test_aota.py
@@ -625,14 +625,13 @@ class TestAOTA(TestCase):
     #@patch("dispatcher.packagemanager.package_manager.get", return_value=Result(200, "ok"))
     #@patch('dispatcher.aota.application_command.get', return_value=Result(200, "ok"))
     @patch('inbm_common_lib.shell_runner.PseudoShellRunner.run', return_value=("", "", 0))
-    @patch('dispatcher.aota.application_command.Application.identify_package', return_value=SupportedDriver.XLINK.value)
-    @patch('dispatcher.aota.application_command.move_file')
-    @patch('os.listdir', return_value=[])
+    #@patch('dispatcher.aota.application_command.Application.identify_package', return_value=SupportedDriver.XLINK.value)
+    #@patch('dispatcher.aota.application_command.move_file')
+    #@patch('os.listdir', return_value=[])
     #@patch('dispatcher.aota.aota_command.AotaCommand.create_repository_cache_repo')
     @patch('dispatcher.aota.factory.is_inside_container', return_value=True, device_reboot="Yes")
     @patch('dispatcher.aota.factory.detect_os', return_value='CentOS')
-    def test_application_centos_driver_update_raise_file_error(self, detect_os, mock_detect_os, listdir, mock_move,
-                                                         support_driver, run):
+    def test_application_centos_driver_update_raise_file_error(self, detect_os, mock_detect_os, run):
         aota = self._build_aota(cmd='update', app_type='application', uri="https://af01p-png.devtools.intel.com/artifactory/turtlecreek-swval-public-png-local/test_files/THB/aota/thb-hddl-xlink-pci-net-driver-dkms_0.1.0-a551d_all.deb")
         with self.assertRaisesRegex(AotaError, "File is not valid"):
             aota.run()
