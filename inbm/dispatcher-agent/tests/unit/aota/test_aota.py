e4598b954afa764a9cb582f2be81f6776ce490c7
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-06-14 17:01:35 +0530

    Adding new test case for file type check

--- inbm/dispatcher-agent/tests/unit/aota/test_aota.py
+++ inbm/dispatcher-agent/tests/unit/aota/test_aota.py
@@ -614,7 +614,7 @@ class TestAOTA(TestCase):
 
     @patch('dispatcher.config_dbs')
     @patch('dispatcher.dispatcher_callbacks')
-    @patch('typing.Mapping')
+    @patch('dispatcher.aota.aota_command.AotaCommand.create_repository_cache_repo')
     def test_application_centos_driver_update_raise_error_if_file_is_not_rpm(self, dispatcher_callbacks: DispatcherCallbacks, parsed_manifest: Mapping[str, Optional[Any]], dbs: ConfigDbs):
         with self.assertRaisesRegex(AotaError, "File is not valid"):
             a = CentOsApplication(dispatcher_callbacks, parsed_manifest, dbs)
