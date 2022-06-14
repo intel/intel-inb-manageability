563936969c189494aee5281efe7401d21be68e53
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-06-14 18:08:59 +0530

    Adding new test case for file type check

--- inbm/dispatcher-agent/tests/unit/aota/test_aota.py
+++ inbm/dispatcher-agent/tests/unit/aota/test_aota.py
@@ -622,7 +622,8 @@ class TestAOTA(TestCase):
             a.update()
 
 
-    @patch("dispatcher.packagemanager.package_manager.get", return_value=Result(200, "ok"))
+    #@patch("dispatcher.packagemanager.package_manager.get", return_value=Result(200, "ok"))
+    @patch('dispatcher.aota.application_command.get', return_value=Result(200, "ok"))
     @patch('inbm_common_lib.shell_runner.PseudoShellRunner.run', return_value=("", "", 0))
     @patch('dispatcher.aota.application_command.Application.identify_package', return_value=SupportedDriver.XLINK.value)
     @patch('dispatcher.aota.application_command.move_file')
@@ -630,8 +631,8 @@ class TestAOTA(TestCase):
     @patch('dispatcher.aota.aota_command.AotaCommand.create_repository_cache_repo')
     @patch('dispatcher.aota.factory.is_inside_container', return_value=True, device_reboot="Yes")
     @patch('dispatcher.aota.factory.detect_os', return_value='CentOS')
-    def test_application_centos_driver_update_raise_file_error(self, get, detect_os, mock_detect_os, create_repo, listdir, mock_move,
-                                                         support_driver, run):
+    def test_application_centos_driver_update_raise_file_error(self, detect_os, mock_detect_os, create_repo, listdir, mock_move,
+                                                         support_driver, mock_get_file, run):
         aota = self._build_aota(cmd='update', app_type='application', uri="http://example/sample.deb")
         with self.assertRaisesRegex(AotaError, "File is not valid"):
             aota.run()
