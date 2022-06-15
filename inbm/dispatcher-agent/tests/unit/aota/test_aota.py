9883b831e7778743ff0f73b2a6113308b3820efc
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-06-15 22:25:37 +0530

    Adding new test case for file type check

--- inbm/dispatcher-agent/tests/unit/aota/test_aota.py
+++ inbm/dispatcher-agent/tests/unit/aota/test_aota.py
@@ -622,8 +622,7 @@ class TestAOTA(TestCase):
             aota.run()
 
 
-    #@patch('dispatcher.aota.application_command.application_repo.get_repo_path', return_value='sample.rpm')
-    @patch('dispatcher.aota.aota_command.DirectoryRepo.get_repo_path', return_value='sample.rpm')
+    #@patch('dispatcher.aota.aota_command.DirectoryRepo.get_repo_path', return_value='sample.rpm')
     #@patch('driver_path', return_value="sample.rpm")
     @patch('inbm_common_lib.shell_runner.PseudoShellRunner.run', return_value=("", "", 0))
     @patch('dispatcher.aota.application_command.Application.identify_package', return_value=SupportedDriver.XLINK.value)
@@ -632,8 +631,9 @@ class TestAOTA(TestCase):
     @patch('dispatcher.aota.aota_command.AotaCommand.create_repository_cache_repo')
     @patch('dispatcher.aota.factory.is_inside_container', return_value=True, device_reboot="Yes")
     @patch('dispatcher.aota.factory.detect_os', return_value='CentOS')
-    def test_application_centos_driver_update_raise_pass(self, detect_os, mock_detect_os, create_repo, listdir, mock_move,
-                                                         support_driver, run, get_rep_path):
+    @patch('dispatcher.aota.application_command.application_repo.get_repo_path', return_value='sample.rpm')
+    def test_application_centos_driver_update_raise_pass(self, get_repo_path, detect_os, mock_detect_os, create_repo, listdir, mock_move,
+                                                         support_driver, run):
         aota = self._build_aota(cmd='update', app_type='application', uri="http://example.com")
         self.assertIsNone(aota.run())
 
