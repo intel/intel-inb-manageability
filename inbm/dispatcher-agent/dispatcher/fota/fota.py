8208b97ed59193ca9f683b5404fd6a5bb0d68299
Siva472 <101618023+Siva472@users.noreply.github.com>
2022-06-16 14:22:19 +0530

    Update fota.py

--- inbm/dispatcher-agent/dispatcher/fota/fota.py
+++ inbm/dispatcher-agent/dispatcher/fota/fota.py
@@ -126,8 +126,8 @@ class FOTA:
             logger.debug(f"guid: {guid}")
             hold_reboot = parse_hold_reboot_flag(self._ota_element)
             logger.debug(f"holdReboot: {hold_reboot}; pkg_filename: {self._pkg_filename}")
-            logger.debug(pkg_filename)
-            logger.debug(signature)
+            logger.debug(bios_vendor)
+            logger.debug(platform_product)
             factory.create_installer(self._repo, FOTA_CONF_PATH, FOTA_CONF_SCHEMA_LOC).\
                 install(guid=guid,
                         tool_options=tool_options,
