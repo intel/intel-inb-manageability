a671da8c0024a4845b0829201d2c84dd5ba399ee
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-06-17 20:39:08 +0530

    Adding new test case for file type check

--- inbm/dispatcher-agent/tests/unit/aota/test_aota.py
+++ inbm/dispatcher-agent/tests/unit/aota/test_aota.py
@@ -618,6 +618,7 @@ class TestAOTA(TestCase):
             aota.run()
 
 
+    @patch('dispatcher.aota.application_command.CentOsApplication.check_file_type', return_value=True)
     @patch('inbm_common_lib.shell_runner.PseudoShellRunner.run', return_value=("", "", 0))
     @patch('dispatcher.aota.application_command.Application.identify_package', return_value=SupportedDriver.XLINK.value)
     @patch('dispatcher.aota.application_command.move_file')
@@ -626,9 +627,9 @@ class TestAOTA(TestCase):
     @patch('dispatcher.aota.factory.is_inside_container', return_value=True, device_reboot="Yes")
     @patch('dispatcher.aota.factory.detect_os', return_value='CentOS')
     def test_application_centos_driver_update_raise_pass(self, detect_os, mock_detect_os, create_repo, listdir, mock_move,
-                                                         support_driver, run):
+                                                         support_driver, run, mock_check_file_type):
         aota = self._build_aota(cmd='update', app_type='application', uri="http://example.com")
-        #self.assertIsNone(aota.run())
+        self.assertIsNone(aota.run())
 
 
 if __name__ == '__main__':
