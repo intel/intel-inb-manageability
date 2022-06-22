2578f1b18aee20b613a252c5f846bda5a79191d4
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-06-22 12:48:28 +0530

    update test_aota.py

--- inbm/dispatcher-agent/tests/unit/aota/test_aota.py
+++ inbm/dispatcher-agent/tests/unit/aota/test_aota.py
@@ -627,7 +627,7 @@ class TestAOTA(TestCase):
     @patch('dispatcher.aota.factory.is_inside_container', return_value=True, device_reboot="Yes")
     @patch('dispatcher.aota.factory.detect_os', return_value='CentOS')
     def test_application_centos_driver_update_raise_pass(self, detect_os, mock_detect_os, create_repo, listdir, mock_move,
-                                                         support_driver, run, mock_check_file_type):
+                                                         support_driver, run, mock_is_rpm_file_type):
         aota = self._build_aota(cmd='update', app_type='application', uri="http://example.com")
         self.assertIsNone(aota.run())
 
