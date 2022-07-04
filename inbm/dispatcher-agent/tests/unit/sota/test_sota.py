7be17cb0381db7f3027b35d19e9494cb518e2bcf
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-07-04 17:26:43 +0530

    Updated sota test case

--- inbm/dispatcher-agent/tests/unit/sota/test_sota.py
+++ inbm/dispatcher-agent/tests/unit/sota/test_sota.py
@@ -178,9 +178,9 @@ class TestSota(testtools.TestCase):
 
     @patch("inbm_lib.detect_os.detect_os")
     def test_check_raise_exception(self, mock_detect_os):
-        parsed_manifest = {'release_date': "1970-01-01"}
-        mock_detect_os.return_value = 'Ubuntu'
-        TestSota.sota_instance._parsed_manifest = parsed_manifest
         #self.assertNotRaises(SotaError, TestSota.sota_instance.check)
         with self.assertRaises(SotaError):
+            parsed_manifest = {'release_date': "1970-01-01"}
+            mock_detect_os.return_value = 'Ubuntu'
+            TestSota.sota_instance._parsed_manifest = parsed_manifest
             TestSota.sota_instance.check()
