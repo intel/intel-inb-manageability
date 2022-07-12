37daf70aaaf6580ac498044aa1b892f2edb5121e
Siva472 <101618023+Siva472@users.noreply.github.com>
2022-07-12 10:21:35 +0530

    Update remediation_manager.py

--- inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
+++ inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
@@ -133,14 +133,14 @@ class RemediationManager:
             if not self.ignore_dbs_results:
                 logger.debug("...........................................................................if not self.ignore_dbs_results")
                 trtl = Trtl(PseudoShellRunner())
-                image_id = None
-                logger.debug(self.dbs_remove_image_on_failed_container)
-                image_id, image_name = self._get_image_id(trtl, container_id)
-                logger.debug(image_id)
-                logger.debug(image_name)
-                if image_id is None:
-                    raise ValueError('Cannot read image ID')
-                self.container_image_list.append(image_name)
+#                 image_id = None
+#                 logger.debug(self.dbs_remove_image_on_failed_container)
+#                 image_id, image_name = self._get_image_id(trtl, container_id)
+#                 logger.debug(image_id)
+#                 logger.debug(image_name)
+#                 if image_id is None:
+#                     raise ValueError('Cannot read image ID')
+#                 self.container_image_list.append(image_name)
 #                 self.dbs_remove_image_on_failed_container = True
                 if self.dbs_remove_image_on_failed_container:
                     logger.debug("....................................................................self.dbs_remove_image_on_failed_container")
