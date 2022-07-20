9fe5edc93a3f5f4f2a9740aa07a2a556bc9764cc
Ms, PurushottamaX <purushottamax.ms@intel.com>
2022-07-20 21:52:39 +0530

    testing

--- inbm/configuration-agent/configuration/broker.py
+++ inbm/configuration-agent/configuration/broker.py
@@ -245,6 +245,9 @@ class Broker:  # pragma: no cover
             list_obj = path_list[i].split(':', 1)
             value = list_obj[1]
             path = list_obj[0]
+            logger.debug(f'@@@@@@@@@@@@@@@@@@@: {UPDATE_CHANNEL}')
+            logger.debug(f'%%%%%%%%%%%%%%%%%%%: {str(path)')
+            
             logger.debug(f'Publishing new value on: {UPDATE_CHANNEL}{str(path)}:{json.dumps(value)}')
             self.mqttc.publish(
                 UPDATE_CHANNEL + str(path), json.dumps(value), retain=True)
