b754fb3771d0b7e5a8700ad01a14cc4af9947699
katha ashok <kathax.ashok.kumar.reddy@intel.com>
2022-07-21 14:45:32 +0530

    update remediation_manager

--- inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
+++ inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
@@ -125,6 +125,11 @@ class RemediationManager:
             if not self.ignore_dbs_results:
                 trtl = Trtl(PseudoShellRunner())
                 image_id = None
+                logger.debug(container_id)
+                image_id, image_name = self._get_image_id(trtl, container_id)
+                logger.debug(image_id)
+                logger.debug(image_name)
+
                 if self.dbs_remove_image_on_failed_container:
                     image_id, image_name = self._get_image_id(trtl, container_id)
                     if image_id is None:
