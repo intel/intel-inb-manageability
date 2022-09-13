5cc5123cc106a305f3346a93be8889124d9e6286
siva472 <thrivendrax.siva.krishnareddy.gangireddy@intel.com>
2022-09-13 15:06:14 +0530

    (HSD 15011730318) Added guid argument for INBC FOTA/POTA command to fix the error: "unrecognized arguments"
                Both positive and negative test case added

--- inbm-lib/tests/unit/inbm_common_lib/test_validater.py
+++ inbm-lib/tests/unit/inbm_common_lib/test_validater.py
@@ -1,6 +1,6 @@
 from unittest import TestCase
 from inbm_common_lib.validater import ConfigurationItem, configuration_bounds_check
-
+import argparse
 
 class TestValidater(TestCase):
 
@@ -21,3 +21,10 @@ class TestValidater(TestCase):
 
     def test_return_value_when_on_lower_limit(self):
         self.assertEquals(1, configuration_bounds_check(self.item, 1))
+
+    def test_check_validate_guid_pass(self):
+        self.assertEquals('6c8e136f-d3e6-4131-ac32-4687cb4abd27', validate_guid('6c8e136f-d3e6-4131-ac32-4687cb4abd27', 'guid',50))
+
+    def test_check_validate_guid_fail(self):
+        with self.assertRaisesRegex(argparse.ArgumentTypeError, 'guid does not support characters other than hexdigits and dash'):
+            validate_guid('6c8e136f-d3e6-4131-ac32-4687cb4abd2@', 'guid',50)
