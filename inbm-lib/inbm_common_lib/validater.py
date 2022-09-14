b180d33cf78fb00e9a27a06ebb20a057b01c2030
siva472 <thrivendrax.siva.krishnareddy.gangireddy@intel.com>
2022-09-14 15:56:40 +0530

    updated testcases4

--- inbm-lib/inbm_common_lib/validater.py
+++ inbm-lib/inbm_common_lib/validater.py
@@ -45,7 +45,7 @@ def validate_guid(value: str) -> str:
         @raise argparse.ArgumentTypeError: Invalid date format
         """
     if not bool(re.match("^[{]?[0-9a-fA-F]{8}" + "-([0-9a-fA-F]{4}-)" + "{3}[0-9a-fA-F]{12}[}]?$", str(value))):
-        raise argparse.ArgumentTypeError(f"GUID should be 36 characters displayed in five groups separated by a dash in the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX")
+        raise argparse.ArgumentTypeError(f"GUID should be 36 characters displayed in five groups separated by a dash in the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX, Hexdigits are not allowed")
     
     return value
 
