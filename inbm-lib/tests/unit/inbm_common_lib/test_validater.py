053c62e46402f331a61b0b94828cdfa1935e63c9
siva472 <thrivendrax.siva.krishnareddy.gangireddy@intel.com>
2022-09-14 14:51:38 +0530

    updated testcases

--- inbm-lib/tests/unit/inbm_common_lib/test_validater.py
+++ inbm-lib/tests/unit/inbm_common_lib/test_validater.py
@@ -27,3 +27,15 @@ class TestValidater(TestCase):
     def test_check_validate_guid_fail(self):
         with self.assertRaisesRegex(argparse.ArgumentTypeError,"GUID should be 36 characters displayed in five groups separated by a dash in the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"):
             validate_guid('6c8e136f-d3e6-4131-ac32-4687cb4abd2@')
+    def test_check_validate_guid_fail(self):
+        with self.assertRaisesRegex(argparse.ArgumentTypeError,"GUID should be 36 characters displayed in five groups separated by a dash in the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"):
+            validate_guid('d3e6-4131-ac32-4687cb4abd27-6c8e136f')
+    def test_check_validate_guid_fail(self):
+        with self.assertRaisesRegex(argparse.ArgumentTypeError,"GUID should be 36 characters displayed in five groups separated by a dash in the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"):
+            validate_guid('4131-ac32-4687cb4abd27-6c8e136f-d3e6')
+    def test_check_validate_guid_fail(self):
+        with self.assertRaisesRegex(argparse.ArgumentTypeError,"GUID should be 36 characters displayed in five groups separated by a dash in the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"):
+            validate_guid('ac32-4687cb4abd27-6c8e136f-d3e6-4131')
+    def test_check_validate_guid_fail(self):
+        with self.assertRaisesRegex(argparse.ArgumentTypeError,"GUID should be 36 characters displayed in five groups separated by a dash in the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX"):
+            validate_guid('4687cb4abd27-6c8e136f-d3e6-4131-ac32')
