bc945905bd5f448cbd1c12ef3ed2c44e8df59a5a
Siva472 <101618023+Siva472@users.noreply.github.com>
2022-09-14 20:06:57 +0530

    Update test_validater.py

--- inbm-lib/tests/unit/inbm_common_lib/test_validater.py
+++ inbm-lib/tests/unit/inbm_common_lib/test_validater.py
@@ -25,10 +25,6 @@ class TestValidater(TestCase):
     def test_check_validate_guid_pass(self):
         self.assertEquals('6c8e136f-d3e6-4131-ac32-4687cb4abd27', validate_guid('6c8e136f-d3e6-4131-ac32-4687cb4abd27')
 
-    def test_check_validate_guid_format(self):
-        with self.assertRaisesRegex(argparse.ArgumentTypeError,"GUID should be 36 characters displayed in five groups separated by a dash in the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX and Hexdigits are allowed"):
-            validate_guid('6c8e136f-d3e6-4131-ac32-4687cb4abd2@')
-
     def test_check_validate_guid_position_substringone(self):
         with self.assertRaisesRegex(argparse.ArgumentTypeError,"GUID should be 36 characters displayed in five groups separated by a dash in the format XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX and Hexdigits are allowed"):
             validate_guid('d3e6-4131-ac32-4687cb4abd27-6c8e136f')
