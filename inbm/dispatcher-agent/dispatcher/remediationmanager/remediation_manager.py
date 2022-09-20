d2caf83e9ffadb87a107198aa51d1a0747924a47
ahameedx <abdulx.hameed@intel.com>
2022-09-20 17:26:08 +0530

    p4

--- inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
+++ inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
@@ -146,18 +146,19 @@ class RemediationManager:
                 logger.info("active_containers_list %s",active_containers_list)
                 if err:
                     logger.error("Error encountered while getting container ID")
-                  
                 
+                if temp_image_name in str(active_containers_list) and not self.dbs_remove_image_on_failed_container:
+                    logger.info("+++> getting appended %s ",temp_image_name )
+                    self.container_image_list_to_be_removed.append(temp_image_name)
+                  
+                 
                 if not temp_image_name in str(active_containers_list) or "DBS" in container_id:
                     self._dispatcher_callbacks.broker_core.telemetry(
                         'DBS Security issue raised on containerID: ' +
                         str(container_id) + ' not present in list.')
                     continue
-
                 
-                if temp_image_name in str(active_containers_list) and not self.dbs_remove_image_on_failed_container:
-                    logger.info("+++> getting appended %s ",temp_image_name )
-                    self.container_image_list_to_be_removed.append(temp_image_name)
+                
 
                 if self.dbs_remove_image_on_failed_container:
                     image_id, image_name = self._get_image_id(trtl, container_id)
