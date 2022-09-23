d8136cbe4322effaf9848c784dc8c98d9d840e6f
siva472 <thrivendrax.siva.krishnareddy.gangireddy@intel.com>
2022-09-23 15:44:02 +0530

    dbs ids check7

--- inbm/dispatcher-agent/tests/unit/remediationmanager/test_remediation_manager.py
+++ inbm/dispatcher-agent/tests/unit/remediationmanager/test_remediation_manager.py
@@ -40,7 +40,7 @@ class TestRemediationManager(TestCase):
     @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
     @patch('inbm_lib.trtl.Trtl.stop_by_id')
     @patch('inbm_lib.trtl.Trtl.remove_container')
-    '''def test_return_container_no_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry, mock_image, mock_remove, mock_list):
+""" def test_return_container_no_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry, mock_image, mock_remove, mock_list):
         try:
             mock_stop_by_id.return_value = (None, None, 0)
             mock_remove_container.return_value = None
@@ -59,8 +59,8 @@ class TestRemediationManager(TestCase):
     @patch('inbm_lib.trtl.Trtl.get_image_by_container_id', return_value=(None, 'cannot find', 3))
     @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
     @patch('inbm_lib.trtl.Trtl.stop_by_id')
-    @patch('inbm_lib.trtl.Trtl.remove_container')'''
-    '''def test_return_container_raises_error_on_bad_format(self, mock_remove_container, mock_stop_by_id,
+    @patch('inbm_lib.trtl.Trtl.remove_container')
+    def test_return_container_raises_error_on_bad_format(self, mock_remove_container, mock_stop_by_id,
                                                          mock_call_telemetry, mock_image, mock_remove, mock_list):
         mock_stop_by_id.return_value = (None, None, 0)
         mock_remove_container.return_value = None
@@ -74,7 +74,7 @@ class TestRemediationManager(TestCase):
     @patch('inbm_lib.trtl.Trtl.get_image_by_container_id', return_value=('abc', None, 0))
     @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
     @patch('inbm_lib.trtl.Trtl.stop_by_id')
-    @patch('inbm_lib.trtl.Trtl.remove_container')'''
+    @patch('inbm_lib.trtl.Trtl.remove_container') """
     def test_telemetry_call_when_stop_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry,
                                              mock_image, mock_remove):
         mock_stop_by_id.return_value = (None, 'error', 1)
