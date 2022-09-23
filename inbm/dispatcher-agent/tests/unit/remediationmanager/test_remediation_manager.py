a3e09c2beb1f45ab83c79d56e9c08c1db136df23
Siva472 <101618023+Siva472@users.noreply.github.com>
2022-09-23 15:51:15 +0530

    Update test_remediation_manager.py

--- inbm/dispatcher-agent/tests/unit/remediationmanager/test_remediation_manager.py
+++ inbm/dispatcher-agent/tests/unit/remediationmanager/test_remediation_manager.py
@@ -40,41 +40,41 @@ class TestRemediationManager(TestCase):
     @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
     @patch('inbm_lib.trtl.Trtl.stop_by_id')
     @patch('inbm_lib.trtl.Trtl.remove_container')
-    """ def test_return_container_no_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry, mock_image, mock_remove, mock_list):
-        try:
-            mock_stop_by_id.return_value = (None, None, 0)
-            mock_remove_container.return_value = None
+#     """ def test_return_container_no_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry, mock_image, mock_remove, mock_list):
+#         try:
+#             mock_stop_by_id.return_value = (None, None, 0)
+#             mock_remove_container.return_value = None
 
-            rm = RemediationManager(self.mock_disp_callbacks_obj)
-            rm.ignore_dbs_results = False
-            rm._remove_container(literal_eval(str(['abc123', 'def234', 'ghi567'])))
-        except ValueError:
-            self.fail("RemediationManager raised ValueError exception unexpectedly!")
-        mock_call_telemetry.assert_called()
-        mock_remove_container.assert_called()
-        mock_stop_by_id.assert_called()
+#             rm = RemediationManager(self.mock_disp_callbacks_obj)
+#             rm.ignore_dbs_results = False
+#             rm._remove_container(literal_eval(str(['abc123', 'def234', 'ghi567'])))
+#         except ValueError:
+#             self.fail("RemediationManager raised ValueError exception unexpectedly!")
+#         mock_call_telemetry.assert_called()
+#         mock_remove_container.assert_called()
+#         mock_stop_by_id.assert_called()
 
-    @patch('inbm_lib.trtl.Trtl.list', return_value=(None, [123, 234, 567]))
-    @patch('inbm_lib.trtl.Trtl.image_remove_by_id', return_value=(None, None, 0))
-    @patch('inbm_lib.trtl.Trtl.get_image_by_container_id', return_value=(None, 'cannot find', 3))
-    @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
-    @patch('inbm_lib.trtl.Trtl.stop_by_id')
-    @patch('inbm_lib.trtl.Trtl.remove_container')
-    def test_return_container_raises_error_on_bad_format(self, mock_remove_container, mock_stop_by_id,
-                                                         mock_call_telemetry, mock_image, mock_remove, mock_list):
-        mock_stop_by_id.return_value = (None, None, 0)
-        mock_remove_container.return_value = None
-        mock_call_telemetry.assert_not_called()
-        with self.assertRaises(ValueError):
-            rm = RemediationManager(self.mock_disp_callbacks_obj)
-            rm.ignore_dbs_results = False
-            rm._remove_container('[123, 234, 567]')
+#     @patch('inbm_lib.trtl.Trtl.list', return_value=(None, [123, 234, 567]))
+#     @patch('inbm_lib.trtl.Trtl.image_remove_by_id', return_value=(None, None, 0))
+#     @patch('inbm_lib.trtl.Trtl.get_image_by_container_id', return_value=(None, 'cannot find', 3))
+#     @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
+#     @patch('inbm_lib.trtl.Trtl.stop_by_id')
+#     @patch('inbm_lib.trtl.Trtl.remove_container')
+#     def test_return_container_raises_error_on_bad_format(self, mock_remove_container, mock_stop_by_id,
+#                                                          mock_call_telemetry, mock_image, mock_remove, mock_list):
+#         mock_stop_by_id.return_value = (None, None, 0)
+#         mock_remove_container.return_value = None
+#         mock_call_telemetry.assert_not_called()
+#         with self.assertRaises(ValueError):
+#             rm = RemediationManager(self.mock_disp_callbacks_obj)
+#             rm.ignore_dbs_results = False
+#             rm._remove_container('[123, 234, 567]')
 
-    @patch('inbm_lib.trtl.Trtl.image_remove_by_id', return_value=(None, None, 0))
-    @patch('inbm_lib.trtl.Trtl.get_image_by_container_id', return_value=('abc', None, 0))
-    @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
-    @patch('inbm_lib.trtl.Trtl.stop_by_id')
-    @patch('inbm_lib.trtl.Trtl.remove_container') """
+#     @patch('inbm_lib.trtl.Trtl.image_remove_by_id', return_value=(None, None, 0))
+#     @patch('inbm_lib.trtl.Trtl.get_image_by_container_id', return_value=('abc', None, 0))
+#     @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
+#     @patch('inbm_lib.trtl.Trtl.stop_by_id')
+#     @patch('inbm_lib.trtl.Trtl.remove_container') """
     def test_telemetry_call_when_stop_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry,
                                              mock_image, mock_remove):
         mock_stop_by_id.return_value = (None, 'error', 1)
