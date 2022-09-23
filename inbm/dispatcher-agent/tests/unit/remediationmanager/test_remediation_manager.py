ba2a267ab22d4f25376881cbbd4a04efd87f4984
siva472 <thrivendrax.siva.krishnareddy.gangireddy@intel.com>
2022-09-23 15:47:30 +0530

    dbs ids check8

--- inbm/dispatcher-agent/tests/unit/remediationmanager/test_remediation_manager.py
+++ inbm/dispatcher-agent/tests/unit/remediationmanager/test_remediation_manager.py
@@ -40,7 +40,7 @@ class TestRemediationManager(TestCase):
     @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
     @patch('inbm_lib.trtl.Trtl.stop_by_id')
     @patch('inbm_lib.trtl.Trtl.remove_container')
-""" def test_return_container_no_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry, mock_image, mock_remove, mock_list):
+    """ def test_return_container_no_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry, mock_image, mock_remove, mock_list):
         try:
             mock_stop_by_id.return_value = (None, None, 0)
             mock_remove_container.return_value = None
