a9a4d611e337fb94e2b995f57075fba7ef3ba129
siva472 <thrivendrax.siva.krishnareddy.gangireddy@intel.com>
2022-09-23 16:24:57 +0530

    dbs ids check9

--- inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
+++ inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
@@ -148,6 +148,8 @@ class RemediationManager:
                     self._dispatcher_callbacks.broker_core.telemetry(
                         'DBS Security issue raised on containerID: ' +
                         str(container_id) + ' not present in list.')
+                    continue
+                
 
                 if temp_image_name in str(active_containers_list) or not self.dbs_remove_image_on_failed_container:
                     self.container_image_list_to_be_removed.append(temp_image_name)
