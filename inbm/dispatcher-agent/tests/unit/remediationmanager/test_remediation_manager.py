8b94cbc9ad2e3c963f4b3fb3923926731a9f1a7e
K <harix.ganesh.k@intel.com>
2022-11-09 15:13:45 +0530

    fixing

--- inbm/dispatcher-agent/tests/unit/remediationmanager/test_remediation_manager.py
+++ inbm/dispatcher-agent/tests/unit/remediationmanager/test_remediation_manager.py
@@ -40,7 +40,7 @@ class TestRemediationManager(TestCase):
     @patch('unit.common.mock_resources.MockDispatcherBroker.telemetry')
     @patch('inbm_lib.trtl.Trtl.stop_by_id')
     @patch('inbm_lib.trtl.Trtl.remove_container')
-    def test_return_container_no_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry, mock_image, mock_remove, mock_list):
+    def test_return_container_no_errors(self, mock_remove_container, mock_stop_by_id, mock_call_telemetry, mock_image, mock_remove):
         try:
             mock_stop_by_id.return_value = (None, None, 0)
             mock_remove_container.return_value = None
@@ -61,7 +61,7 @@ class TestRemediationManager(TestCase):
     @patch('inbm_lib.trtl.Trtl.stop_by_id')
     @patch('inbm_lib.trtl.Trtl.remove_container')
     def test_return_container_raises_error_on_bad_format(self, mock_remove_container, mock_stop_by_id,
-                                                         mock_call_telemetry, mock_image, mock_remove, mock_list):
+                                                         mock_call_telemetry, mock_image, mock_remove,):
         mock_stop_by_id.return_value = (None, None, 0)
         mock_remove_container.return_value = None
         mock_call_telemetry.assert_not_called()
