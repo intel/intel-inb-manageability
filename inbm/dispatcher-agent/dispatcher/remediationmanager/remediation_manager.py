5c0e58447122811da59cc5784b669421f7a782e5
K <harix.ganesh.k@intel.com>
2022-11-10 11:07:02 +0530

    FixI

--- inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
+++ inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
@@ -75,8 +75,8 @@ class RemediationManager:
         for image_id in ids:
             if image_id in self.container_image_list_to_be_removed:
                 self._remove_single_image(image_id)
-			logger.debug("image id...")
-            #else:
+				logger.debug("image id...")
+            else:
                 self._dispatcher_callbacks.broker_core.telemetry('DBS Security issue raised on imageID: '
                                                                  + str(image_id)
                                                                  + '.  Image is not present in container image list.')
