892c800775eb9f1da349f9d9a3b6e1f24207794b
K <harix.ganesh.k@intel.com>
2022-11-10 11:45:47 +0530

    FIXING

--- inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
+++ inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
@@ -75,7 +75,7 @@ class RemediationManager:
         for image_id in ids:
             if image_id in self.container_image_list_to_be_removed:
                 self._remove_single_image(image_id)
-				logger.debug(image_id)
+                logger.debug(image_id)
             else:
                 self._dispatcher_callbacks.broker_core.telemetry('DBS Security issue raised on imageID: '
                                                                  + str(image_id)
