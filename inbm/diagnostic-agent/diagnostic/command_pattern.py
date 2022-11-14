181b1d9fb0f5ee033840af518377286035dc07e0
Gairuboina Sirisha <sirishax.gairuboina@intel.com>
2022-11-14 17:52:26 +0530

    new change

--- inbm/diagnostic-agent/diagnostic/command_pattern.py
+++ inbm/diagnostic-agent/diagnostic/command_pattern.py
@@ -123,7 +123,7 @@ class NetworkChecker(Command):
             #         'message'] = 'Network interfaces down.  Cannot find network interface with a default route.'
             #     logger.debug(self._result)
             #     return self._result
-            else 'default' in gateways and len(gateways['default']) = 0:
+            else 'default' in gateways or len(gateways['default']) = 0:
                 logger.debug("*****************OS_Rolling_else******************")
                 self._result['rc'] = 1
                 self._result['message'] = 'Network interfaces down.  Cannot find network interface with a default route.'
