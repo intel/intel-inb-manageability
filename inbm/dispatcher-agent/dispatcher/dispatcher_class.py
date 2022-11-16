58f2baf08317aa4c63e2851073b3d34c39e826a4
Gairuboina Sirisha <sirishax.gairuboina@intel.com>
2022-11-16 10:37:54 +0530

    Added n/w check for OS rollback2

--- inbm/dispatcher-agent/dispatcher/dispatcher_class.py
+++ inbm/dispatcher-agent/dispatcher/dispatcher_class.py
@@ -881,7 +881,7 @@ class Dispatcher(WindowsService):
             In case of a good health report, it just deletes the snapshot."""
             try:
                 self.install_check(check_type='swCheck')
-                #self.install_check(check_type='check_network')
+                self.install_check(check_type='check_network')
                 self._telemetry('On Boot, Diagnostics reports healthy system')
                 self.invoke_sota(action='diagnostic_system_healthy', snapshot=None)
             except DispatcherException:
