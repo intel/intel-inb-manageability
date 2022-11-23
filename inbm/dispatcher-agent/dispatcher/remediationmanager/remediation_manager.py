21f785d28f9cd3da2eb2af0105b7e52092ae9518
Gairuboina Sirisha <sirishax.gairuboina@intel.com>
2022-11-23 20:15:19 +0530

    added changes

--- inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
+++ inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
@@ -72,9 +72,12 @@ class RemediationManager:
         logger.debug("Removing Images...")
         for image_id in ids:
             logger.debug(len(ids))
-            new_image_id = image_id + ":latest"
-            if new_image_id in self.container_image_list_to_be_removed:
-                self._remove_single_image(image_id)
+            new_image_id = image_id.split(':')
+            print(image_id)
+            print(new_image_id)
+            print(self.container_image_list_to_be_removed)
+            if image_id in self.container_image_list_to_be_removed:
+                self._remove_single_image(new_image_id[0])
                 #self._remove_single_image(image_id)
             else:
                 self._dispatcher_callbacks.broker_core.telemetry('DBS Security issue raised on imageID: '
