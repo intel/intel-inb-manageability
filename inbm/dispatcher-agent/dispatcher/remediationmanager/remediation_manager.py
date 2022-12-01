80ee6f8e0f3829fd49283390e0049bb0238fd11c
Gairuboina Sirisha <sirishax.gairuboina@intel.com>
2022-12-01 12:20:22 +0530

    commented Ashok temp one logic

--- inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
+++ inbm/dispatcher-agent/dispatcher/remediationmanager/remediation_manager.py
@@ -138,11 +138,11 @@ class RemediationManager:
                 if err:
                     logger.error("Error encountered while getting container ID")
 
-                if not temp_image_name in str(active_containers_list) or "DBS" in container_id:
-                    self._dispatcher_callbacks.broker_core.telemetry(
-                        'DBS Security issue raised on containerID: ' +
-                        str(container_id) + ' not present in list.')
-                    continue
+                #if not temp_image_name in str(active_containers_list) or "DBS" in container_id:
+                #    self._dispatcher_callbacks.broker_core.telemetry(
+                #        'DBS Security issue raised on containerID: ' +
+                #        str(container_id) + ' not present in list.')
+                #    continue
 
                 if temp_image_name in str(active_containers_list) and not self.dbs_remove_image_on_failed_container:
                     self.container_image_list_to_be_removed.append(temp_image_name)
