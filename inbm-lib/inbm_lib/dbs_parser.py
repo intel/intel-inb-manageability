bfd03d7d8fe74e78b2968384b8c7ca0f0a7fa063
Gairuboina Sirisha <sirishax.gairuboina@intel.com>
2022-12-02 18:36:04 +0530

    4

--- inbm-lib/inbm_lib/dbs_parser.py
+++ inbm-lib/inbm_lib/dbs_parser.py
@@ -50,28 +50,28 @@ def parse_docker_bench_security_results(dbs_output: str) -> Dict[str, Union[bool
     logger.debug(failed_containers)
     for line in dbs_output.splitlines():
         logger.debug("############################# dbs_parser6 #################################")
-        logger.debug(line)
+        #logger.debug(line)
         logger.debug(prev_warn)
         if _is_name_in_line(line, prev_warn):
             logger.debug("############################# dbs_parser7 #################################")
-            logger.debug(line)
+            #logger.debug(line)
             logger.debug(failed_containers)
             logger.debug(failed_images)
             _fetch_names_for_warn_test(line, failed_containers, failed_images)
             logger.debug("############################# dbs_parser8 #################################")
-            logger.debug(line)
+            #logger.debug(line)
             logger.debug(failed_containers)
             logger.debug(failed_images)
         if _is_test_warn(line):
             logger.debug("############################# dbs_parser9 #################################")
-            logger.debug(line)
-            logger.debug(fails)
+            #logger.debug(line)
+            #logger.debug(fails)
             fails = _add_test_in_fails(line, fails)
             logger.debug("############################# dbs_parser10 #################################")
-            logger.debug(line)
-            logger.debug(fails)
+            #logger.debug(line)
+            #logger.debug(fails)
             logger.debug(success_flag)
-            logger.debug(prev_warn)
+            #logger.debug(prev_warn)
             success_flag = False
             prev_warn = True
             continue
@@ -83,7 +83,7 @@ def parse_docker_bench_security_results(dbs_output: str) -> Dict[str, Union[bool
         logger.debug(failed_images)
         logger.debug(failed_containers)
         logger.debug(result)
-        logger.debug(fails)
+        #logger.debug(fails)
     return {'success_flag': success_flag,
             'failed_images': failed_images,
             'failed_containers': failed_containers,
