3c167d2b00644d06f3cb81d7a975089da9d16299
Gairuboina Sirisha <sirishax.gairuboina@intel.com>
2022-12-02 16:06:12 +0530

    out

--- inbm/diagnostic-agent/diagnostic/docker_bench_security_runner.py
+++ inbm/diagnostic-agent/diagnostic/docker_bench_security_runner.py
@@ -50,6 +50,7 @@ class DockerBenchRunner(Thread):
             logger.debug(self.failed_image_list)
             logger.debug(self.result_string)
             logger.debug(self.result)
+            logger.debug(out)
             logger.debug("############################    out run   ############################")
 
     @staticmethod
