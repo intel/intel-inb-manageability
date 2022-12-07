c09e7a9d3e71aa14786a838d10d480b0047b698d
Gairuboina Sirisha <sirishax.gairuboina@intel.com>
2022-12-07 10:56:22 +0530

    selflock

--- inbm/diagnostic-agent/diagnostic/docker_bench_security_runner.py
+++ inbm/diagnostic-agent/diagnostic/docker_bench_security_runner.py
@@ -25,6 +25,7 @@ class DockerBenchRunner(Thread):
         logger.debug(line.strip())
     def __init__(self):
         Thread.__init__(self, name="dockerBenchRunner")
+        self.lock = Lock()
         self.result = None
         self.result_string = None
         self.failed_image_list = None
@@ -35,9 +36,11 @@ class DockerBenchRunner(Thread):
         for line in traceback.format_stack():
             logger.debug(line.strip())
         #self.lock = Lock()
+        self.lock.acquire()
         out = Trtl(PseudoShellRunner()).run_docker_bench_security_test()
+        self.lock.release()
         #self.lock.acquire()
-        lock.acquire()
+        #lock.acquire()
         logger.debug("############################    out       ############################")
         logger.debug(out)
         logger.debug("############################    out run   ############################")
@@ -82,7 +85,7 @@ class DockerBenchRunner(Thread):
         logger.debug("############# output #################################")
         logger.debug(output)
         #self.lock.release()
-        lock.release()
+        #lock.release()
         parse_result = parse_docker_bench_security_results(output)
         logger.debug("############# parse_result #################################")
         logger.debug(parse_result)
