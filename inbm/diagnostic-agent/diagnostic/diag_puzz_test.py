8cc6648ac123afd4cc560804f4c927b65540b319
K <harix.ganesh.k@intel.com>
2022-12-12 12:19:15 +0530

    ADDED NEW FILE

---
+++ inbm/diagnostic-agent/diagnostic/diag_puzz_test.py
@@ -0,0 +1,16 @@
+import logging
+from abc import ABC
+
+##import netifaces
+##import psutil
+import os
+from typing import Dict, Union, Any
+
+##from .filesystem_utilities import get_free_space
+##from .util import get_free_memory
+##from inbm_common_lib.utility import get_canonical_representation_of_path
+##from inbm_lib.constants import TRTL_PATH
+##from inbm_lib.trtl import Trtl
+##from inbm_common_lib.shell_runner import PseudoShellRunner
+from command_pattern import DeviceBatteryHealthChecker
+
