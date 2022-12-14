417445015e2ed5b864a9ca7eee3cdf383db0720c
K <harix.ganesh.k@intel.com>
2022-12-14 15:28:18 +0530

    ADDED NEW FILE

--- inbm/diagnostic-agent/diagnostic/diag_puzz_test.py
+++ inbm/diagnostic-agent/diagnostic/diag_puzz_test.py
@@ -1,16 +1,19 @@
+import atheris
+import sys
+import os
+sys.path.append(os.path.join(os.path.dirname(__file__), "../"))
+
 import logging
 from abc import ABC
 
-##import netifaces
-##import psutil
 import os
 from typing import Dict, Union, Any
 
-##from .filesystem_utilities import get_free_space
-##from .util import get_free_memory
-##from inbm_common_lib.utility import get_canonical_representation_of_path
-##from inbm_lib.constants import TRTL_PATH
-##from inbm_lib.trtl import Trtl
-##from inbm_common_lib.shell_runner import PseudoShellRunner
+with atheris.instrument_imports():
+ from unittest import TestCase
+ 
+ 
+
+@atheris.instrument_func
 from .command_pattern import DeviceBatteryHealthChecker
 
