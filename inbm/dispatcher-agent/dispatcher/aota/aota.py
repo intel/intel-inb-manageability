0a6d9c14518bce19dfe869b28661e74ec05d8611
Gairuboina Sirisha <sirishax.gairuboina@intel.com>
2022-12-21 17:29:01 +0530

    applied lock

--- inbm/dispatcher-agent/dispatcher/aota/aota.py
+++ inbm/dispatcher-agent/dispatcher/aota/aota.py
@@ -17,7 +17,8 @@ from dispatcher.dispatcher_callbacks import DispatcherCallbacks
 from .factory import get_app_instance
 from .aota_error import AotaError
 from .cleaner import cleanup_repo
-import time
+#import time
+import Lock
 logger = logging.getLogger(__name__)
 
 
@@ -31,6 +32,7 @@ class AOTA:
 
     def __init__(self, dispatcher_callbacks: DispatcherCallbacks, parsed_manifest: Mapping[str, Optional[Any]],
                  dbs: ConfigDbs) -> None:
+        self.lock = Lock()
         # security assumption: parsed_manifest is already validated
         self._dispatcher_callbacks = dispatcher_callbacks
         self._cmd = parsed_manifest['cmd']
@@ -59,9 +61,11 @@ class AOTA:
 
             app_method = getattr(self._app_instance, self._cmd)
             app_method()
-            time.sleep(0.1)
+            #time.sleep(5)
+            self.lock.acquire()
             self._dispatcher_callbacks.broker_core.telemetry(
                 f'AOTA {self._app_type} {self._cmd} {COMMAND_SUCCESS}')
+            self.lock.release()
             self._app_instance.cleanup()
         except (AotaError, UrlSecurityException) as e:
             err = f"AOTA {self._app_type} {self._cmd} FAILED: {e}"
