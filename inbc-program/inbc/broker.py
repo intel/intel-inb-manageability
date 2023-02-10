6e9b3d2de943a73c40a9936e33f1f88724b852f7
Natalie Gaston <natalie.gaston@intel.com>
2023-02-10 09:14:18 -0800

    try long sleep to see if thats the issue

--- inbc-program/inbc/broker.py
+++ inbc-program/inbc/broker.py
@@ -79,7 +79,7 @@ class Broker(IBroker):
 
             print('Subscribe to: {0}'.format(EVENT_CHANNEL))
             self.mqttc.subscribe(EVENT_CHANNEL, self._on_event)
-            time.sleep(1) #time for teh channel to subscribe
+            time.sleep(3)  # time for the channel to subscribe
             logger.debug('Setting up broker success.')
         except Exception as exception:
             logger.exception('Subscribe failed: %s', exception)
