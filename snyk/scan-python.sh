5a226913c7319793eaecb44114333ac16c00aaa3
Natalie Gaston <natalie.gaston@intel.com>
2023-02-13 13:37:46 -0800

    Synk 3.0.14 Release

--- snyk/scan-python.sh
+++ snyk/scan-python.sh
@@ -9,5 +9,7 @@ AGENT_NAME="$2"
 cp -r "$AGENT_PATH" /"$AGENT_NAME"
 cd /"$AGENT_NAME"
 if [ -f requirements.txt ]; then pip3 install -r requirements.txt >&2; fi
+pip3 uninstall -y inbm-lib
+snyk monitor --org=$SNYK_ORG --project-name=iotg-inb-"$AGENT_NAME" /"$AGENT_NAME" -d
 snyk test --org=$SNYK_ORG --project-name=iotg-inb-"$AGENT_NAME" /"$AGENT_NAME" -d --json | snyk-to-html \
          -t $(npm config get prefix)/lib/node_modules/snyk-to-html/template/test-cve-report.hbs
