84b49360b4fcb7f588019fe6d44c7bf72c638b6f
Gavin Lewis <gavin.b.lewis@intel.com>
2023-02-16 13:12:48 -0800

    Remove Bit Creek from Snyk scanning script

--- snyk/run.sh
+++ snyk/run.sh
@@ -46,11 +46,6 @@ for i in dispatcher diagnostic cloudadapter configuration telemetry ; do
     scan_python /repository/inbm/$i-agent $i-agent >"$DIR"/results/snyk-$i-agent.html || fail=1
 done
 
-for i in vision node ; do
-    scan_python /repository/inbm-vision/$i-agent $i-agent  >"$DIR"/results/snyk-$i-agent.html || fail=1
-done
-
-scan_python /repository/inbm-vision/flashless-program flashless-program  >"$DIR"/results/snyk-flashless-program.html || fail=1
 scan_python /repository/inbc-program inbc-program >"$DIR"/results/snyk-inbc-program.html || fail=1
 
 
