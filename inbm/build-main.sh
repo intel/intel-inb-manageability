2d0a5ef94697a4fc4b479165a1634c9052982d25
gblewis1 <gavin.b.lewis@intel.com>
2023-04-26 14:27:17 -0700

    Add UCC-specific build with only needed agents (#307)
    * Refactor and modify UCC flow tests
    * Add UCC-specific build with only needed agents

--- inbm/build-main.sh
+++ inbm/build-main.sh
@@ -13,4 +13,12 @@ cp -r debs-20.04/*simulator* "$DIR"/output-main/ )
 ( cd "$DIR"/output-main ;
   TAR_CONTENTS="./*-agent*.deb trtl*.deb tpm-provision*.deb mqtt*.deb inbc*.deb"
     tar zcvf Intel-Manageability.preview.tar.gz $TAR_CONTENTS && \
-	      rm -f $TAR_CONTENTS )
+  UCC_CONTENTS="./*cloudadapter*-agent*.deb tpm-provision*.deb mqtt*.deb inbc*.deb"
+    tar zcvf Intel-Manageability.ucc.tar.gz $UCC_CONTENTS && \
+	      rm -f $TAR_CONTENTS $UCC_CONTENTS && \
+  mkdir ucc && \
+  mv Intel-Manageability.ucc.tar.gz ucc && \
+  cp install-tc.sh ucc/install-tc-ucc.sh && \
+  sed -i '1a export UCC_MODE=true' ucc/install-tc-ucc.sh && \
+  cp uninstall-tc.sh ucc && \
+  cp ../LICENSE ucc )
