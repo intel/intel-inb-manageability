d3d83c4d331a9b6263b0f9c094f3be5c6c2360d4
Gavin Lewis <gavin.b.lewis@intel.com>
2024-01-03 11:00:49 -0800

    Add scaffolding for integration tests for inbc source

---
+++ inbm/integration-reloaded/test/source/SOURCE.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+set -x
+
+source /scripts/test_util.sh
+
+trap 'kill -9 $(jobs -p) || true'  EXIT
+
+test_failed() {
+   echo "Return code: $?"
+   echo "TEST FAILED!!!"
+}
+trap test_failed ERR
+
+echo "Starting source test." | systemd-cat
+
+inbc source os list
+inbc source application list
+# add more cases here
