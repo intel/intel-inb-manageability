6531240bc1a5a23a3f703febc83fe254ca02df22
Gavin Lewis <gavin.b.lewis@intel.com>
2024-01-10 12:43:24 -0800

    fix quoting

--- inbm/integration-reloaded/test/aota/AOTA_UPDATE_SIGNATURE_PREBOOT.sh
+++ inbm/integration-reloaded/test/aota/AOTA_UPDATE_SIGNATURE_PREBOOT.sh
@@ -19,7 +19,7 @@ test_echo AOTA APLICATION UPDATE SIGNATURE
 
 dpkg --purge sample-application || true
 
-XML="<?xml version="1.0" encoding="utf-8"?><manifest><type>ota</type><ota><header><type>aota</type><repo>remote</repo></header><type><aota><cmd>update</cmd><app>application</app><fetch>http://127.0.0.1:80/sample-application-1.0-1.deb</fetch><signature>`python3 /scripts/create_signature.py ${VAGRANT_INPUT_PATH}/succeed_rpm_key.pem ${VAGRANT_INPUT_PATH}/sample-application-1.0-1.deb 1234`</signature><sigversion>384</sigversion><deviceReboot>yes</deviceReboot><version>0</version></aota></type></ota></manifest>"
+XML="<?xml version=\"1.0\" encoding=\"utf-8\"?><manifest><type>ota</type><ota><header><type>aota</type><repo>remote</repo></header><type><aota><cmd>update</cmd><app>application</app><fetch>http://127.0.0.1:80/sample-application-1.0-1.deb</fetch><signature>`python3 /scripts/create_signature.py ${VAGRANT_INPUT_PATH}/succeed_rpm_key.pem ${VAGRANT_INPUT_PATH}/sample-application-1.0-1.deb 1234`</signature><sigversion>384</sigversion><deviceReboot>yes</deviceReboot><version>0</version></aota></type></ota></manifest>"
 
 trigger_ota "${XML}"
 
