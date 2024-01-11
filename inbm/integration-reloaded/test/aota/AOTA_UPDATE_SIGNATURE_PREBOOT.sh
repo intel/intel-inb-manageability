e4917d9f192472dc0ea9e73df55b933e3c4d2787
Gavin Lewis <gavin.b.lewis@intel.com>
2024-01-11 14:03:49 -0800

    update inbc-program to pass in signature + integration test to test

--- inbm/integration-reloaded/test/aota/AOTA_UPDATE_SIGNATURE_PREBOOT.sh
+++ inbm/integration-reloaded/test/aota/AOTA_UPDATE_SIGNATURE_PREBOOT.sh
@@ -13,27 +13,12 @@ trap test_failed ERR
 
 echo "Starting APPLICATION UPDATE SIGNATURE preboot test." | systemd-cat
 
-echo cp...
 cp /scripts/succeed_rpm_cert.pem /etc/intel-manageability/public/dispatcher-agent/ota_signature_cert.pem
 echo chmod...
 chmod a+r /vagrant/nginx-data/*
 
-echo purge...
 test_echo AOTA APLICATION UPDATE SIGNATURE
 
 dpkg --purge sample-application || true
 
-echo XML with signature..
-XML="<?xml version=\"1.0\" encoding=\"utf-8\"?><manifest><type>ota</type><ota><header><type>aota</type><repo>remote</repo></header><type><aota><cmd>update</cmd><app>application</app><fetch>http://127.0.0.1:80/sample-application-1.0-1.deb</fetch><signature>`python3 /scripts/create_signature.py ${VAGRANT_INPUT_PATH}/succeed_rpm_key.pem ${VAGRANT_INPUT_PATH}/sample-application-1.0-1.deb 1234`</signature><sigversion>384</sigversion><deviceReboot>yes</deviceReboot><version>0</version></aota></type></ota></manifest>"
-
-echo trigger OTA...
-trigger_ota "${XML}"
-
-if listen_event | grep Rebooting... ; then
-  echo AOTA APPLICATION UPDATE SIGNATURE preboot test good so far.
-else
-  echo Error in AOTA APPLICATION UPDATE SIGNATURE test preboot.  Showing recent journalctl.
-  journalctl -a --no-pager -n 50
-  exit 1
-fi
-
+inbc aota --uri http://127.0.0.1:80/sample-application-1.0-1.deb -a "application" -c "update" --signature `python3 /scripts/create_signature.py ${VAGRANT_INPUT_PATH}/succeed_rpm_key.pem ${VAGRANT_INPUT_PATH}/sample-application-1.0-1.deb 1234`
