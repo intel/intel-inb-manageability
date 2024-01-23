ffb22452bd999304d5b9ae60741386e7d0304b62
Gen AI Copilot <141839948+nex-maximus@users.noreply.github.com>
2024-01-23 23:36:25 +0530

    updating inbm/dispatcher-agent/dispatcher/source/ubuntu_source_manager_genai_unit_test.py[skip ci]

--- inbm/dispatcher-agent/dispatcher/source/ubuntu_source_manager_genai_unit_test.py
+++ inbm/dispatcher-agent/dispatcher/source/ubuntu_source_manager_genai_unit_test.py
@@ -1,11 +1,11 @@
 import sys
+sys.path.insert(0, '../../../')
 sys.path.insert(0, '../../')
 sys.path.insert(0, '../../../../')
-sys.path.insert(0, './')
-sys.path.insert(0, '../../../')
 sys.path.insert(0, '../')
-sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm/dispatcher-agent/')
+sys.path.insert(0, './')
 sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm/dispatcher-agent/dispatcher/source/')
+sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm/dispatcher-agent/')
 sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm-lib/')
 
 if __name__ == '__main__':
