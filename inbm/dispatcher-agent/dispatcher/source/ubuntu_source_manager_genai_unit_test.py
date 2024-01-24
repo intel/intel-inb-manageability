95eef49476e9ddc6d56d823250d2bf0f94df9920
Gen AI Copilot <141839948+nex-maximus@users.noreply.github.com>
2024-01-24 23:27:46 +0530

    updating inbm/dispatcher-agent/dispatcher/source/ubuntu_source_manager_genai_unit_test.py[skip ci]

--- inbm/dispatcher-agent/dispatcher/source/ubuntu_source_manager_genai_unit_test.py
+++ inbm/dispatcher-agent/dispatcher/source/ubuntu_source_manager_genai_unit_test.py
@@ -1,9 +1,9 @@
 import sys
-sys.path.insert(0, '../../../')
-sys.path.insert(0, '../')
-sys.path.insert(0, '../../')
 sys.path.insert(0, './')
+sys.path.insert(0, '../../')
+sys.path.insert(0, '../../../')
 sys.path.insert(0, '../../../../')
+sys.path.insert(0, '../')
 sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm/dispatcher-agent/dispatcher/source/')
 sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm/dispatcher-agent/')
 sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm-lib/')
