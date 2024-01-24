164f8d5ec3d2eca752671794b00ebf148bf5652f
Gen AI Copilot <141839948+nex-maximus@users.noreply.github.com>
2024-01-24 23:28:01 +0530

    updating inbm/dispatcher-agent/dispatcher/source/ubuntu_source_manager_genai_unit_test.py[skip ci]

--- inbm/dispatcher-agent/dispatcher/source/ubuntu_source_manager_genai_unit_test.py
+++ inbm/dispatcher-agent/dispatcher/source/ubuntu_source_manager_genai_unit_test.py
@@ -1,12 +1,12 @@
 import sys
-sys.path.insert(0, './')
 sys.path.insert(0, '../../')
-sys.path.insert(0, '../../../')
 sys.path.insert(0, '../../../../')
 sys.path.insert(0, '../')
+sys.path.insert(0, './')
+sys.path.insert(0, '../../../')
 sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm/dispatcher-agent/dispatcher/source/')
-sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm/dispatcher-agent/')
 sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm-lib/')
+sys.path.insert(0, '/home/runner/GITHUB_ACTION_RUNNERS/_work/intel-inb-manageability/intel-inb-manageability/inbm/dispatcher-agent/')
 import pytest
 from unittest.mock import patch, MagicMock
 from dispatcher.source.ubuntu_source_manager import UbuntuApplicationSourceManager, ApplicationAddSourceParameters
