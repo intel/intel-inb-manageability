1d3ded64b14f31232bb4d8d1a12589b9cb675a42
Gavin Lewis <gavin.b.lewis@intel.com>
2024-05-01 19:09:07 -0700

    Revert "Add INBS cloud + ping command to Cloudadapter (#496)"
    This will be removed from 4.2.3 since we need to go through some
    internal process to approve it. It should land in the next release.
    
    This reverts commit 4c5ee47c9f3a288ace0debc564a0f7e36b4cef1d.

--- inbm/format-python.sh
+++ inbm/format-python.sh
@@ -2,4 +2,4 @@
 
 set -euxo pipefail
 
-for agent in *-agent ; do autopep8 --max-line-length 99 --exclude '*_pb2*' --in-place -r "$agent" ; done
+for agent in *-agent ; do autopep8 --max-line-length 99 --in-place -r "$agent" ; done
