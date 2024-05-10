f7eb222c2d9a1b8568a05a7bf90d47c0e082ac83
yengliong <yeng.liong.wong@intel.com>
2024-05-10 10:29:20 +0800

    Fix dispatcher unable to access sql database

--- inbm/dispatcher-agent/dispatcher/schedule/sqlite_manager.py
+++ inbm/dispatcher-agent/dispatcher/schedule/sqlite_manager.py
@@ -5,57 +5,51 @@
     SPDX-License-Identifier: Apache-2.0
 """
 
+import logging
 import sqlite3
 from sqlite3 import Error
 from typing import Optional
 from .scheduled_task import ScheduledTask
 from dispatcher.dispatcher_exception import DispatcherException
 
+logger = logging.getLogger(__name__)
+
+
 class SqliteManager:
     def __init__(self, db_file) -> None:
         self._db_file = db_file
-        self._conn = self._create_connection()
-        self.create_table()
-            
-    def _create_connection(self) -> sqlite3.Connection:
-        """ create a database connection to a SQLite database """
-        conn = None
-        try:
-            conn = sqlite3.connect(self._db_file)
-        except (sqlite3.OperationalError, sqlite3.InternalError) as e:
-            raise DispatcherException(f"Error connecting to database: {e}")
-        return conn
-
-    def create_table(self) -> None:
-        """
-        Method to create a table in the database.
-        """
-        try:
-            cur = self._conn.cursor()
-            # Check if the table already exists, if not, create the table
-            cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='task'")
-            if cur.fetchone() is None:
-                sql = ''' CREATE TABLE task (
-                            start_time text,
-                            end_time text,
-                            manifest text
-                        ); '''
-                self._conn.execute(sql)  # Execute the SQL command
-        except (sqlite3.OperationalError, sqlite3.InternalError) as e:
-            raise DispatcherException(f"Error creating table in database: {e}")
 
     def create_task(self, task: ScheduledTask) -> None:
         """
         Create a new scheduled task
         :param task: ScheduledTask object
         """
+        conn = None
         try:
-            with self._conn:
-                sql = ''' INSERT INTO task(start_time, end_time, manifest)
-                        VALUES(?,?,?) '''
-                self._conn.execute(sql, (task.start_time, task.end_time, task.manifest))
+            conn = sqlite3.connect(self._db_file)
+            # Creating the table
+            cur = conn.cursor()
+            # Check if the table already exists, if not, create the table
+            logger.debug("Checking if the table exist...")
+            cur.execute("SELECT name FROM sqlite_master WHERE type='table' AND name='task'")
+            if cur.fetchone() is None:
+                logger.debug("Table not exist. Creating the table.")
+                sql = ''' CREATE TABLE task (
+                            start_time text,
+                            end_time text,
+                            manifest text
+                        ); '''
+                conn.execute(sql)  # Execute the SQL command
+
+            # Add the task
+            sql = ''' INSERT INTO task(start_time, end_time, manifest)
+                      VALUES(?,?,?) '''
+            logger.debug(f"Adding task: {str(task.start_time), str(task.end_time), str(task.manifest)}")
+            cur.execute(sql, (str(task.start_time), str(task.end_time), str(task.manifest)))
+            conn.commit()
+            logger.debug("Task added.")
         except (sqlite3.OperationalError, sqlite3.InternalError) as e:
             raise DispatcherException(f"Error connecting to database: {e}")
         finally:
-            if self._conn:
-                self._conn.close()
+            if conn:
+                conn.close()
