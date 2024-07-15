adda3c27cdeda041a443781b59def3c5a882c0c0
yengliong <yeng.liong.wong@intel.com>
2024-07-16 02:57:21 +0800

    This PR adds the functionality to records a package level granular update status data and store it at /var/cache/manageability/inbm-update-log.log. (#524)
    The dispatcher will check the package information such as package status and version after SOTA and records it.
    The granular log file will be deleted by PUA.
    
    This PR also introduces a new OTA status called NO_UPDATE_AVAILABLE. This state is set when SOTA success without update (no new packages).

--- inbm/dispatcher-agent/dispatcher/update_logger.py
+++ inbm/dispatcher-agent/dispatcher/update_logger.py
@@ -5,12 +5,17 @@
     SPDX-License-Identifier: Apache-2.0
 """
 
+import os
 import json
 import datetime
 import logging
-from typing import Optional
+from typing import Optional, Dict, List, Any
 
-from inbm_lib.constants import LOG_FILE, OTA_PENDING, FORMAT_VERSION
+from inbm_lib.constants import LOG_FILE, GRANULAR_LOG_FILE, SYSTEM_HISTORY_LOG_FILE, OTA_PENDING, FORMAT_VERSION, \
+    SOTA, OS, APPLICATION, PACKAGE_INSTALL, PACKAGE_UPGRADE
+
+from inbm_lib.package_info import get_package_start_date, extract_package_names_and_versions, check_package_status, \
+    check_package_version
 
 logger = logging.getLogger(__name__)
 
@@ -20,11 +25,13 @@ class UpdateLogger:
        log file.
     @param ota_type: Type of OTA
     @param data: meta-data of the OTA
+    @param package_list: list of packages installed by dispatcher
     """
 
     def __init__(self, ota_type: str, data: str) -> None:
         self.status = ""
         self.ota_type = ota_type
+        self.package_list: str = ""
         self._time = datetime.datetime.now()
         self.metadata = data
         self.error = ""
@@ -70,4 +77,118 @@ class UpdateLogger:
                 return log_file.read()
         except OSError as e:
             logger.error(f'Error {e} on opening the file {LOG_FILE}')
+            return None
+
+    def save_granular_log_file(self) -> None:
+        """Add package level granular update status data to the granular log file"""
+        logger.debug("")
+        # If the granular log file doesn't exist, create it. The file will be deleted by PUA when update done/failed.
+        if not os.path.exists(GRANULAR_LOG_FILE):
+            logger.debug(f"File not exist. Creating the {GRANULAR_LOG_FILE}...")
+            data: Dict[str, List[Any]] = {
+                "UpdateLog": []
+            }
+            with open(GRANULAR_LOG_FILE, "w") as file:
+                json.dump(data, file)
+        if self.ota_type == SOTA:
+            if self.package_list != "":
+                # SOTA package installation with package list
+                self.update_granular_with_sota_package_list_install()
+            else:
+                # Treat it as regular sota package upgrade
+                self.update_granular_with_sota_package_upgrade()
+        else:
+            logger.debug(f"Unsupported ota_type:{self.ota_type} to record granular data.")
+
+    def update_granular_with_sota_package_upgrade(self) -> None:
+        """This function checks the latest upgrade in history log file and extract the package information.
+           The package information such as package name and version will be stored in granular log file.
+        """
+        log_data = self.read_history_log_file()
+        if log_data:
+            # Get the latest upgrade history
+            # Split the log into parts
+            parts = log_data.split('\n\n')
+            # Filter parts that contain 'upgrade'
+            upgrade_parts = [part for part in parts if 'upgrade' in part.lower()]
+            latest_upgrade = upgrade_parts[-1]
+            update_time = get_package_start_date(latest_upgrade)
+            package_dict = extract_package_names_and_versions(latest_upgrade)
+
+            # Load current data in granular log file.
+            with open(GRANULAR_LOG_FILE, 'r') as f:
+                data = json.load(f)
+
+            # Create the package information and store it
+            for package_name, version in package_dict.items():
+                logger.debug(f"Package: {package_name}, Version: {version}")
+
+                # Get status using dpkg-query
+                status = check_package_status(package_name)
+
+                package_info = {
+                    "update_type": OS,
+                    "package_name": package_name,
+                    "update_time": update_time,
+                    "action": PACKAGE_UPGRADE,
+                    "status": status,
+                    "version": version
+                }
+                # Remove previous entries with the same package_name from the UpdateLog list
+                data['UpdateLog'] = [log for log in data['UpdateLog'] if
+                                     log['package_name'] != package_info['package_name']]
+                # Append the new package information to the UpdateLog list
+                data['UpdateLog'].append(package_info)
+
+            # Open the file in write mode to save the updated data
+            with open(GRANULAR_LOG_FILE, 'w') as f:
+                json.dump(data, f, indent=4)
+
+
+    def update_granular_with_sota_package_list_install(self) -> None:
+        """This function checks the latest package installation information using dpkg-query command.
+           It checks all the packages inside package_list and records the package's status and version.
+           These information will be stored in granular log file.
+        """
+        # Load current data in granular log file.
+        with open(GRANULAR_LOG_FILE, 'r') as f:
+            data = json.load(f)
+
+        packages = self.package_list.split(',')
+        for package_name in packages:
+            #  Get status
+            status = check_package_status(package_name)
+
+            # Get Version
+            version = check_package_version(package_name)
+
+            package_info = {
+                "update_type": APPLICATION,
+                "package_name": package_name,
+                "update_time": self._time.isoformat(),
+                "action": PACKAGE_INSTALL,
+                "status": status,
+                "version": version
+            }
+
+            # Remove previous entries with the same package_name from the UpdateLog list
+            data['UpdateLog'] = [log for log in data['UpdateLog'] if
+                                 log['package_name'] != package_info['package_name']]
+            # Append the latest package information to the UpdateLog list
+            data['UpdateLog'].append(package_info)
+
+            # Open the file in write mode to save the updated data
+            with open(GRANULAR_LOG_FILE, 'w') as f:
+                json.dump(data, f, indent=4)
+
+    def read_history_log_file(self) -> Optional[str]:
+        """Read the apt history log file.
+
+        @return: content of the history log file
+        """
+        try:
+            with open(SYSTEM_HISTORY_LOG_FILE, 'r') as log_file:
+                return log_file.read()
+        except OSError as e:
+            logger.error(f'Error {e} on opening the file {SYSTEM_HISTORY_LOG_FILE}')
             return None
