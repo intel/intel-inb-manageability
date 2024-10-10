f3b234331a8804add44fe847c0887655c5433269
Sirlapu, Tejaswini <tejaswini.sirlapu@intel.com>
2024-10-10 14:53:25 -0700

    Changes and unit tests

--- inbm/dispatcher-agent/dispatcher/schedule/rpc_activate_operation.py
+++ inbm/dispatcher-agent/dispatcher/schedule/rpc_activate_operation.py
@@ -5,25 +5,27 @@
     SPDX-License-Identifier: Apache-2.0
 """
 from typing import Optional
+import shlex
 from inbm_common_lib.shell_runner import PseudoShellRunner
 
 
 
 class RpcActivateOperation: 
-    def __init__(self, url: str, profile_name: str) -> str:
+    def __init__(self) -> None:
         """RpcActivateOperation class is to execute rpc activate command
-
-        @param url: url address of the RPS
-        @param profile_name: profile_name used for rpc configuration
         """
         pass
 
     def execute_rpc_activation_cmd(self, url, name) -> str:
         """Executes the RPC activation command.
 
+        @param url: url address of the RPS
+        @param name: profile name used for rpc configuration
         @return: return 'success' if execution succeeds else returns 'Failure'
         """
-        command = f"./rpc activate -u {url}/activate -n --profile {name}"
+        url = shlex.quote(url)
+        name = shlex.quote(name)
+        command = f"rpc activate -u {url}/activate -n --profile {name}"
         (out, err, code) = PseudoShellRunner().run(command)
         if code == 0 and 'CIRA: Configured' in out:
             return "Success"
