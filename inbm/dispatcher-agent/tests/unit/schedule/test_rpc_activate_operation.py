974e4380c318106da9cd75108f91a5cccf7bff2d
Sirlapu, Tejaswini <tejaswini.sirlapu@intel.com>
2024-10-10 15:19:57 -0700

    Fix unit test parameters

--- inbm/dispatcher-agent/tests/unit/schedule/test_rpc_activate_operation.py
+++ inbm/dispatcher-agent/tests/unit/schedule/test_rpc_activate_operation.py
@@ -19,10 +19,10 @@ class TestRpcActivateOperation(TestCase):
 
     @patch("inbm_common_lib.shell_runner.PseudoShellRunner.run", return_value=('Success', "", 0))
     def test_execute_rpc_activation_cmd_success(self, mock_run) -> None:
-        rpc_result = RpcActivateOperation(self).execute_rpc_activation_cmd(self.url, self.name)
+        rpc_result = RpcActivateOperation().execute_rpc_activation_cmd(self.url, self.profile_name)
         self.assertEqual(rpc_result, 'Success')
 
     @patch("inbm_common_lib.shell_runner.PseudoShellRunner.run", return_value=('Failure', "", 0))
     def test_execute_rpc_activation_cmd_failure(self, mock_run) -> None:
-        rpc_result = RpcActivateOperation(self).execute_rpc_activation_cmd(self.url, self.name)
+        rpc_result = RpcActivateOperation().execute_rpc_activation_cmd(self.url, self.profile_name)
         self.assertEqual(rpc_result, 'Failure')
