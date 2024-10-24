c2294c6780e9dc0d785ec934dcbf66cd1fb1e67b
yengliong <yeng.liong.wong@intel.com>
2024-10-24 17:21:30 +0800

    Change release server token path
    This PR changes the release server token path from
    /etc/intel_edge_node/tokens/platform-update-agent/rs_access_token to
    /etc/intel_edge_node/tokens/release-service/rs_access_token.
    
    Signed-off-by: yengliong <yeng.liong.wong@intel.com>

--- inbm/dispatcher-agent/dispatcher/sota/constants.py
+++ inbm/dispatcher-agent/dispatcher/sota/constants.py
@@ -18,7 +18,7 @@ MENDER_ARTIFACT_PATH = get_canonical_representation_of_path("/etc/mender/artifac
 TIBER_UPDATE_TOOL_PATH = get_canonical_representation_of_path('/usr/bin/os-update-tool.sh')
 
 # Release server access token path
-RELEASE_SERVER_TOKEN_PATH = get_canonical_representation_of_path('/etc/intel_edge_node/tokens/platform-update-agent/'
+RELEASE_SERVER_TOKEN_PATH = get_canonical_representation_of_path('/etc/intel_edge_node/tokens/release-service/'
                                                        'rs_access_token')
 
 SOTA_STATE = 'normal'
