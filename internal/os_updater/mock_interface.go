a08e993d4821deaa7b9671eb0a12924516a35531
yengliong <yeng.liong.wong@intel.com>
2025-03-21 11:31:16 +0800

    Fix unit test

--- internal/os_updater/mock_interface.go
+++ internal/os_updater/mock_interface.go
@@ -13,52 +13,52 @@ import (
 
 // MockDownloader is a mock implementation of the Downloader interface.
 type MockDownloader struct {
-    DownloadFunc func() error
+	DownloadFunc func() error
 }
 
-// Download calls the DownloadFunc.	
+// Download calls the DownloadFunc.
 func (m *MockDownloader) Download() error {
-    return m.DownloadFunc()
+	return m.DownloadFunc()
 }
 
 // MockUpdater is a mock implementation of the Updater interface.
 type MockUpdater struct {
-    UpdateFunc func() error
+	UpdateFunc func() error
 }
 
 // Update calls the UpdateFunc.
 func (m *MockUpdater) Update() error {
-    return m.UpdateFunc()
+	return m.UpdateFunc()
 }
 
 // MockRebooter is a mock implementation of the Rebooter interface.
 type MockRebooter struct {
-    RebootFunc func() error
+	RebootFunc func() error
 }
 
 // Reboot calls the RebootFunc.
 func (m *MockRebooter) Reboot() error {
-    return m.RebootFunc()
+	return m.RebootFunc()
 }
 
 // MockUpdaterFactory is a mock implementation of the UpdaterFactory interface.
 type MockUpdaterFactory struct {
-    CreateDownloaderFunc func(*pb.UpdateSystemSoftwareRequest) Downloader
-    CreateUpdaterFunc    func(utils.Executor, *pb.UpdateSystemSoftwareRequest) Updater
-    CreateRebooterFunc   func() Rebooter
+	CreateDownloaderFunc func(*pb.UpdateSystemSoftwareRequest) Downloader
+	CreateUpdaterFunc    func(utils.Executor, *pb.UpdateSystemSoftwareRequest) Updater
+	CreateRebooterFunc   func(utils.Executor, *pb.UpdateSystemSoftwareRequest) Rebooter
 }
 
 // CreateDownloader calls the CreateDownloaderFunc.
 func (m *MockUpdaterFactory) CreateDownloader(req *pb.UpdateSystemSoftwareRequest) Downloader {
-    return m.CreateDownloaderFunc(req)
+	return m.CreateDownloaderFunc(req)
 }
 
 // CreateUpdater calls the CreateUpdaterFunc.
 func (m *MockUpdaterFactory) CreateUpdater(cmdExec utils.Executor, req *pb.UpdateSystemSoftwareRequest) Updater {
-    return m.CreateUpdaterFunc(cmdExec, req)
+	return m.CreateUpdaterFunc(cmdExec, req)
 }
 
 // CreateRebooter calls the CreateRebooterFunc.
-func (m *MockUpdaterFactory) CreateRebooter() Rebooter {
-    return m.CreateRebooterFunc()
+func (m *MockUpdaterFactory) CreateRebooter(cmdExec utils.Executor, req *pb.UpdateSystemSoftwareRequest) Rebooter {
+	return m.CreateRebooterFunc(cmdExec, req)
 }
